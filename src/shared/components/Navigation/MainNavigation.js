4fcb67af9e83d21768254d079a8991f8b53d9f70
Craig Caparaz <cappuccino1890@Craigs-MacBook-Air.local>
2023-05-21 22:21:33 -0700

    fixing navbar

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 import "./MainNavigation.css";
 
-const MainNavigation = props => {
-    return <MainHeader>
-        <button className="main-navigation__menu-btn">
-            <span />
-            <span />
-            <span />
-            </button>
-            <h1 className="main-navigation__title">
-               <Link to="/">Your Places</Link>
-            </h1>
-            <nav>
-                <NavLinks />
-            </nav>
+const MainNavigation = (props) => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+    const openDrawer = () => {
+        setDrawerIsOpen(true);
+    }
+    return (
+  <React.Fragment>
+   { drawerIsOpen && <SideDrawer>
+      <nav className="main-navigation__drawer-nav">
+        <NavLinks />
+      </nav>
+    </SideDrawer>}
+    <MainHeader>
+      <button className="main-navigation__menu-btn" onClick={openDrawer}>
+        <span />
+        <span />
+        <span />
+      </button>
+      <h1 className="main-navigation__title">
+        <Link to="/">Your Places</Link>
+      </h1>
+      <nav className="main-navigation__header-nav">
+        <NavLinks />
+      </nav>
     </MainHeader>
-}
+  </React.Fragment>
+    )
+};
 
 export default MainNavigation;
